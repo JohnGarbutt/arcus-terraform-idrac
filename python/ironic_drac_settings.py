7cec20a526ad8f2bb6443397c02b4bca3f354ccf
John Garbutt <johng@stackhpc.com>
2020-07-07 23:40:30 +0000

    Print all settings

--- python/ironic_drac_settings.py
+++ python/ironic_drac_settings.py
@@ -4,16 +4,26 @@ import json
 
 import dracclient.client
 
+
+def to_dict(settings):
+    settings_dict = {}
+    for name, settings_attr in settings.items():
+        settings_dict[name] = settings_attr.__dict__
+    return settings_dict
+
+
+def get_all_settings(client):
+    bios = to_dict(client.list_bios_settings())
+    idrac = to_dict(client.list_idrac_settings())
+    nics = client.list_nics()
+    nic1 = to_dict(client.list_nic_settings("NIC.Embedded.1-1-1"))
+    all_settings = {"bios": bios, "idrac": idrac, "nics": nics, "nic1": nic1}
+    return all_settings
+
+
 client = dracclient.client.DRACClient(
     host="10.202.100.137",
     username="root",
     password="calvin")
 
-settings = client.list_bios_settings()
-settings_dict = {}
-for name, settings_attr in settings.items():
-    settings_dict[name] = settings_attr.__dict__
-print(json.dumps(settings_dict, indent=2))
-
-#print(client.list_idrac_settings())
-#print(client.list_nic_settings())
+print(json.dumps(get_all_settings(client), indent=2))
