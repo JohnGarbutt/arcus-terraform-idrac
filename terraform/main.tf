8927eb79401d7161562c20c3c124c319e83e5263
John Garbutt <john@johngarbutt.com>
2020-07-07 09:29:27 +0100

    Fix typos, add example entry

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,24 @@ provider "openstack" {
   version = "~> 1.29"
 }
 
+provider "local" {
+  version = "~> 1.4"
+}
+
+# Example entry in parsed csv:
+# [{
+#    "bmc_mac_format" = "6C:2B:59:00:00:00"
+#    "device_name" = "sv1-ab07-u2"
+#    "device_serial" = "AABBCC12"
+#    "management_mac_format" = "6C:2B:59:00:00:01"
+#    "datacentre_id" = "AC-H3"
+#    "rack" = "AB07"
+#    "rack_pos" = "2"
+#  }]
 locals {
-  idrac_mapping = yamldecode(file("idrac.csv"))
+  idrac_mapping = csvdecode(file("idrac.csv"))
 }
 
 output idrac {
-  value = idrac_mapping
+  value = local.idrac_mapping
 }
