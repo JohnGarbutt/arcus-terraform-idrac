92570d73d9a9ebc317a41a4d5d97a02a14b45632
John Garbutt <johng@stackhpc.com>
2020-07-08 00:27:44 +0000

    Restructure so a loop will be easier

--- python/ironic_drac_settings.py
+++ python/ironic_drac_settings.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-import pprint
 import json
+import pprint
+import sys
 
 import dracclient.client
+import openstack
 
 
 def to_dict(settings):
@@ -22,39 +24,59 @@ def get_all_settings(client):
     return all_settings
 
 
+def update_settings(client):
+    jobs = client.list_jobs(only_unfinished=True)
+    if len(jobs) > 0:
+        pprint.pprint(jobs)
+        exit(-1)
+
+    bios_settings = {
+      "LogicalProc": "Disabled",
+      "SysProfile": "PerfOptimized",
+      "SetBootOrderEn": "NIC.Embedded.1-1-1,HardDisk.List.1-1",
+    }
+    bios_result = client.set_bios_settings(bios_settings)
+    print(bios_result)
+
+    idrac_settings = {
+      "IPMILan.1#Enable": "Enabled",
+    }
+    idrac_result = client.set_idrac_settings(idrac_settings)
+    print(idrac_result)
+
+    reboot_required = bios_result['is_reboot_required']
+    if bios_result['is_commit_required']:
+        reboot_required = bios_result['is_reboot_required']
+        print(client.commit_pending_bios_changes(reboot=reboot_required))
+
+    if idrac_result['is_commit_required']:
+        reboot_required = idrac_result['is_reboot_required']
+        print(client.commit_pending_idrac_changes(reboot=reboot_required))
+
+    pprint.pprint(client.list_jobs(only_unfinished=True))
+
+
+def find_idrac_ports(conn):
+    return [{
+            "name": raw_port["name"],
+            "mac": raw_port["mac_address"],
+            "ip": raw_port["fixed_ips"][0]["ip_address"],
+            "rack": [tag for tag in raw_port["tags"] if "DR" in tag][0],
+            "datacentre": [tag for tag in raw_port["tags"] if "DC" in tag][0],
+        }
+        for raw_port in conn.network.ports(tags="iDRAC")]
+
+
+openstack.enable_logging(True, stream=sys.stdout)
+conn = openstack.connection.from_config(cloud="arcus", debug=True)
+
+ips = [idrac["ip"] for idrac in find_idrac_ports(conn)]
+print(ips)
+
 client = dracclient.client.DRACClient(
     host="10.202.100.137",
     username="root",
     password="calvin")
-
 #print(json.dumps(get_all_settings(client), indent=2))
+update_settings(client)
 
-jobs = client.list_jobs(only_unfinished=True)
-if len(jobs) > 0:
-    pprint.pprint(jobs)
-    exit(-1)
-
-bios_settings = {
-  "LogicalProc": "Disabled",
-  "SysProfile": "PerfOptimized",
-  "SetBootOrderEn": "NIC.Embedded.1-1-1,HardDisk.List.1-1,InfiniBand.Slot.4-1,InfiniBand.Slot.4-2",
-}
-bios_result = client.set_bios_settings(bios_settings)
-print(bios_result)
-
-idrac_settings = {
-  "IPMILan.1#Enable": "Enabled",
-}
-idrac_result = client.set_idrac_settings(idrac_settings)
-print(idrac_result)
-
-reboot_required = bios_result['is_reboot_required']
-if bios_result['is_commit_required']:
-    reboot_required = bios_result['is_reboot_required']
-    print(client.commit_pending_bios_changes(reboot=reboot_required))
-
-if idrac_result['is_commit_required']:
-    reboot_required = idrac_result['is_reboot_required']
-    print(client.commit_pending_idrac_changes(reboot=reboot_required))
-
-pprint.pprint(client.list_jobs(only_unfinished=True))
